95d07d9207ca7a12641c11f200488c76b9c678ec
madoos <maurice.ronet.dominguez@gmail.com>
2016-12-14 21:11:44 +0100

    refactorized app structure

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
 'use strict';
 
-
 const express = require('express');
 const path = require('path')
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, './assets')));
-app.use('/dist', express.static(path.join(__dirname, './dist')));
+app.use('/assets', express.static(path.join(__dirname, '../public/assets')));
+app.use('/dist', express.static(path.join(__dirname, '../public/dist')));
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(3000, function () {
